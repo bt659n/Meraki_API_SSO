347c7fad1376cc3ff07b98c57a742ebd6a4f4ae6
bt659n <bt659n@gmail.com>
2026-06-10 13:38:44 +0800

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(console.error);
-
-// 插件启动时自动缓存 API 文档
-chrome.runtime.onInstalled.addListener(() => {
-    fetch("https://raw.githubusercontent.com/meraki/openapi/master/openapi/spec3.json")
-    .then(res => res.json())
-    .then(spec => chrome.storage.local.set({ 'merakiSpec': spec }))
-    .catch(console.error);
-});
